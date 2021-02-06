0983299ad0f6530142a2a6520562daf3cd4ad07e
amb1s1 <amb1s1@gmail.com>
2021-02-06 18:14:08 +0000

    1.fixed the sleep line to be use as intended.\n2.jump to the zsh prompt at the end.

--- intial_install.sh
+++ intial_install.sh
@@ -8,11 +8,12 @@ else
   echo "I have no Idea what im doing here"
 fi
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+sleep 1s
 sudo chsh -s $(which zsh) $USER
 git clone https://github.com/lifepillar/vim-solarized8.git ~/.vim/pack/themes/opt/solarized8
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-sleep 3
+sleep 1s
 touch ~/.vimrc
 touch ~/.tmux.conf
 touch ~/.zshrc
@@ -20,4 +21,4 @@ ln -sf ~/dotfile/vimrc ~/.vimrc
 ln -sf ~/dotfile/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfile/zshrc ~/.zshrc
 vim +PluginInstall +qall
-source ~/.zshrc
+zsh
