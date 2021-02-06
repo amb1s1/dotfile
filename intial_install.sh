aebf2deb2df14adb4c1797ca1addab1f99f168a0
amb1s1 <amb1s1@gmail.com>
2021-02-06 17:50:32 +0000

    Added line to change the default shell to zsh

--- intial_install.sh
+++ intial_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+USER=$(whoami)
 if command -v apt-get >/dev/null; then
 	sudo apt-get install git zsh wget curl vim tmux -y
 elif command -v yum >/dev/null; then
@@ -8,6 +8,7 @@ else
   echo "I have no Idea what im doing here"
 fi
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+sudo chsh -s $(which zsh) $USER
 git clone https://github.com/lifepillar/vim-solarized8.git ~/.vim/pack/themes/opt/solarized8
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
