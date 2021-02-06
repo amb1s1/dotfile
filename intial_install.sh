24fab409ed21194d16026812ef9fc4ecd0086514
amb1s1 <amb1s1@gmail.com>
2021-02-06 18:32:06 +0000

    I added the installation of build essential tools. Added tmux and essential tools to RHEL devices

--- intial_install.sh
+++ intial_install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 USER=$(whoami)
 if command -v apt-get >/dev/null; then
-	sudo apt-get install git zsh wget curl vim tmux -y
+	sudo apt-get install git zsh wget curl vim tmux build-essential -y
 elif command -v yum >/dev/null; then
-	sudo yum install git zsh wget curl vim -y
+	sudo yum install git zsh wget curl vim tmux -y
+    sudo yum groupinstall 'Development Tools' -y
 else
   echo "I have no Idea what im doing here"
 fi
@@ -22,3 +23,4 @@ ln -sf ~/dotfile/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfile/zshrc ~/.zshrc
 vim +PluginInstall +qall
 zsh
+vim +PluginUpdate +qall
